1b09fd7b8bb2958df15e75e0264a1b36c09e9736
Daniel Metzing <daniel.metzing@centralway.com>
2018-03-06 21:51:14 +0100

    Working example, need to extend groups

--- Source/SwiftLintFramework/Rules/ModifiersOrderRule.swift
+++ Source/SwiftLintFramework/Rules/ModifiersOrderRule.swift
@@ -10,21 +10,10 @@ import Foundation
 import SourceKittenFramework
 
 // TODO: Get somehow `SwiftDeclarationAttribute` for Swift 4.1
-public enum SwiftDeclarationAttributeKind: String, SwiftLangSyntax  {
-    case `ibaction`         = "source.decl.attribute.ibaction"
-    case `iboutlet`         = "source.decl.attribute.iboutlet"
-    case `ibdesignable`     = "source.decl.attribute.ibdesignable"
-    case `ibinspectable`    = "source.decl.attribute.ibinspectable"
-    case `objc`             = "source.decl.attribute.objc"
+public enum SwiftDeclarationAttributeKind: String, SwiftLangSyntax  { // TODO: Need to check what happens with `class` and `static`
+    case NSManaged          = "source.decl.attribute.NSManaged"
     case name               = "source.decl.attribute.objc.name"
     case available          = "source.decl.attribute.available"
-    case `final`            = "source.decl.attribute.final"
-    case `required`         = "source.decl.attribute.required"
-    case `optional`         = "source.decl.attribute.optional"
-    case noreturn           = "source.decl.attribute.noreturn"
-    case `NSManaged`        = "source.decl.attribute.NSManaged"
-    case `lazy`             = "source.decl.attribute.lazy"
-    case `dynamic`          = "source.decl.attribute.dynamic"
     case infix              = "source.decl.attribute.infix"
     case prefix             = "source.decl.attribute.prefix"
     case postfix            = "source.decl.attribute.postfix"
@@ -32,53 +21,63 @@ public enum SwiftDeclarationAttributeKind: String, SwiftLangSyntax  {
     case noescape           = "source.decl.attribute.noescape"
     case nonobjc            = "source.decl.attribute.nonobjc"
     case objcMembers        = "source.decl.attribute.objcMembers"
+    case privateSetter      = "source.decl.attribute.setter_access.private"
+    case internalSetter     = "source.decl.attribute.setter_access.internal"
+    case publicSetter       = "source.decl.attribute.setter_access.public"
+    case `ibaction`         = "source.decl.attribute.ibaction"
+    case `iboutlet`         = "source.decl.attribute.iboutlet"
+    case `ibdesignable`     = "source.decl.attribute.ibdesignable"
+    case `ibinspectable`    = "source.decl.attribute.ibinspectable"
+    case `objc`             = "source.decl.attribute.objc" // TODO: This always has to be first. How to handle this case?
+    case `final`            = "source.decl.attribute.final"
+    case `required`         = "source.decl.attribute.required"
+    case `optional`         = "source.decl.attribute.optional"
+    case noreturn           = "source.decl.attribute.noreturn"
+    case `lazy`             = "source.decl.attribute.lazy"
+    case `dynamic`          = "source.decl.attribute.dynamic"
     case `mutating`         = "source.decl.attribute.mutating"
     case `nonmutating`      = "source.decl.attribute.nonmutating"
     case `convenience`      = "source.decl.attribute.convenience"
     case `override`         = "source.decl.attribute.override"
     case `weak`             = "source.decl.attribute.weak"
     case `testable`         = "source.decl.attribute.testable"
-    case privateSetter      = "source.decl.attribute.setter_access.private"
     case `public`           = "source.decl.attribute.public"
     case `private`          = "source.decl.attribute.private"
     case `internal`         = "source.decl.attribute.internal"
     case `open`             = "source.decl.attribute.open"
 }
 
-extension SwiftDeclarationAttributeKind {
-    static var ACLs: [SwiftDeclarationAttributeKind] {
-        return [.private,
-                .internal,
-                .public,
-                .open]
-    }
-
-    static var mutators: [SwiftDeclarationAttributeKind] {
-        return [.mutating,
-                .nonmutating]
-    }
-
-    static var overrides: [SwiftDeclarationAttributeKind] {
-        return [.override]
->>>>>>> Stashed changes
-    }
-}
-
 // TODO: Get the possible groups of attribute kinds
 public enum SwiftDeclarationAttributeKindGroup {
     case acl
+    case setterACL
     case mutators
+    case memoryReference
     case `override`
 
     var swiftDeclarationAttributeKinds: [SwiftDeclarationAttributeKind] {
         switch self {
         case .acl:
-            return SwiftDeclarationAttributeKind.ACLs
+            return [.private,
+                    .internal,
+                    .public,
+                    .open]
+        case .setterACL:
+            return [.privateSetter,
+                    .internalSetter,
+                    .publicSetter]
         case .mutators:
-            return SwiftDeclarationAttributeKind.mutators
+            return [.mutating,
+                    .nonmutating]
+        case .memoryReference:
+            return [.weak]
         case .override:
-            return SwiftDeclarationAttributeKind.overrides
+            return [.override]
         }
+    }
+
+    static var allValues: [SwiftDeclarationAttributeKindGroup] {
+        return [.acl, .setterACL, .mutators, .override]
     }
 }
 
@@ -88,12 +87,13 @@ public struct ModifiersOrderRule: ASTRule, OptInRule, ConfigurationProviderRule 
     public static let description = RuleDescription(
         identifier: "modifiers_order",
         name: "Modifiers Order",
-        description: "Modifiers order should be consistent.", kind: RuleKind.style,
+        description: "Modifiers order should be consistent.",
+        kind: .style,
         nonTriggeringExamples: [
             "@objc \n override public private(set) weak var foo: Bar?\n @objc \npublic final class MyClass: NSObject {\n }",
             "@objc \npublic final class MyClass: NSObject {\n" +
-            "private final func myFinal() {}\n" +
-            "weak var myWeak: NSString? = nil\n" +
+                "private final func myFinal() {}\n" +
+                "weak var myWeak: NSString? = nil\n" +
             "public static let nnumber = 3 \n }",
 
             "public final class MyClass {}"
@@ -137,38 +137,50 @@ public struct ModifiersOrderRule: ASTRule, OptInRule, ConfigurationProviderRule 
                          kind: SwiftDeclarationKind,
                          dictionary: [String: SourceKitRepresentable]) -> [StyleViolation] {
 
-        guard observedDeclarationKinds.contains(kind) else {
+        guard observedDeclarationKinds.contains(kind),
+              let offset = dictionary.offset else {
             return []
         }
 
-        let enclosedSwiftAttributesWithMetaData = dictionary.enclosedSwiftAttributesWithMetaData
-        let aclIndex = findIndex(of: .acl, in: enclosedSwiftAttributesWithMetaData)
-        print("\(String(describing: aclIndex))")
-        // TODO: 1. Get indexes that could be in front of ACL
-        //       2. Check if any of them is after ACL or not
+        let preferedOrderOfModifiers = configuration.beforeACL
+                                       .compactMap { return group(of: $0) }
+                                       + [.acl]
+                                       + configuration.afterACL
+                                       .compactMap { return group(of: $0) }
+
+        for (index, group) in preferedOrderOfModifiers.enumerated() {
+            guard let groupIndex = findIndex(of: group, in: dictionary.enclosedSwiftAttributesWithMetaData) else { continue }
+            if groupIndex != index {
+                return [StyleViolation(ruleDescription: type(of: self).description,
+                                       severity: configuration.severityConfiguration.severity,
+                                       location: Location(file: file, byteOffset: offset))]
+            }
+        }
+
         return []
     }
 
-    func findIndex(of group: SwiftDeclarationAttributeKindGroup, in declarationAttributes: [[String: SourceKitRepresentable]]) -> Int? {
-        let ordered = order(declarationAttributes: declarationAttributes)
-        let modifiers = ordered.compactMap { attributeOrMetaData -> SwiftDeclarationAttributeKind? in
-            if let attribute = attributeOrMetaData["key.attribute"] as? String {
+    private func findIndex(of group: SwiftDeclarationAttributeKindGroup, in declarationAttributes: [[String: SourceKitRepresentable]]) -> Int? {
+        return declarationAttributes
+            .sorted {
+                guard let rhsOffset = $0["key.offset"] as? Int64, let lhsOffset = $1["key.offset"] as? Int64 else { return false }
+                return rhsOffset < lhsOffset
+            }
+            .compactMap { modifierOrMetaData -> SwiftDeclarationAttributeKind? in
+                guard let attribute = modifierOrMetaData["key.attribute"] as? String else { return nil }
                 return SwiftDeclarationAttributeKind(rawValue: attribute)
             }
-            return nil
-        }
-
-        let groupIndex = modifiers.index { return group.swiftDeclarationAttributeKinds.contains($0) }
-        return groupIndex
+            .index {
+                group.swiftDeclarationAttributeKinds.contains($0)
+            }
     }
 
-    private func order(declarationAttributes: [[String: SourceKitRepresentable]]) ->  [[String: SourceKitRepresentable]] {
-        return declarationAttributes.sorted(by: { rhs, lhs in
-            guard let rhsOffset = rhs["key.offset"] as? Int64,
-                let lhsOffset = lhs["key.offset"] as? Int64 else {
-                    return false
+    private func group(of rawAttribute: String) -> SwiftDeclarationAttributeKindGroup? {
+        for value in SwiftDeclarationAttributeKindGroup.allValues {
+            for attributeKind in value.swiftDeclarationAttributeKinds where attributeKind.rawValue.hasSuffix(rawAttribute) {
+                return value
             }
-            return rhsOffset < lhsOffset
-        })
+        }
+        return nil
     }
 }
