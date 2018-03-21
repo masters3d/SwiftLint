90ec17f052b2865078c1a2c54bc655e05d24b5c7
Daniel Metzing <daniel.metzing@centralway.com>
2018-03-21 18:28:47 +0100

    Adding support for static and class -> needs to discuss if approach is ok

--- Source/SwiftLintFramework/Rules/ModifiersOrderRule.swift
+++ Source/SwiftLintFramework/Rules/ModifiersOrderRule.swift
@@ -10,7 +10,7 @@ import Foundation
 import SourceKittenFramework
 
 // TODO: Get somehow `SwiftDeclarationAttribute` for Swift 4.1
-public enum SwiftDeclarationAttributeKind: String, SwiftLangSyntax  { // TODO: Need to check what happens with `class` and `static`
+public enum SwiftDeclarationAttributeKind: String, SwiftLangSyntax  {
     case NSManaged          = "source.decl.attribute.NSManaged"
     case name               = "source.decl.attribute.objc.name"
     case available          = "source.decl.attribute.available"
@@ -21,14 +21,14 @@ public enum SwiftDeclarationAttributeKind: String, SwiftLangSyntax  { // TODO: N
     case noescape           = "source.decl.attribute.noescape"
     case nonobjc            = "source.decl.attribute.nonobjc"
     case objcMembers        = "source.decl.attribute.objcMembers"
+    case `objc`             = "source.decl.attribute.objc"
     case privateSetter      = "source.decl.attribute.setter_access.private"
     case internalSetter     = "source.decl.attribute.setter_access.internal"
     case publicSetter       = "source.decl.attribute.setter_access.public"
-    case `ibaction`         = "source.decl.attribute.ibaction"
-    case `iboutlet`         = "source.decl.attribute.iboutlet"
-    case `ibdesignable`     = "source.decl.attribute.ibdesignable"
-    case `ibinspectable`    = "source.decl.attribute.ibinspectable"
-    case `objc`             = "source.decl.attribute.objc" // TODO: This always has to be first. How to handle this case?
+    case ibaction           = "source.decl.attribute.ibaction"
+    case iboutlet           = "source.decl.attribute.iboutlet"
+    case ibdesignable       = "source.decl.attribute.ibdesignable"
+    case ibinspectable      = "source.decl.attribute.ibinspectable"
     case `final`            = "source.decl.attribute.final"
     case `required`         = "source.decl.attribute.required"
     case `optional`         = "source.decl.attribute.optional"
@@ -45,15 +45,24 @@ public enum SwiftDeclarationAttributeKind: String, SwiftLangSyntax  { // TODO: N
     case `private`          = "source.decl.attribute.private"
     case `internal`         = "source.decl.attribute.internal"
     case `open`             = "source.decl.attribute.open"
+
+    // TODO: Discuss the option of these "fake" cases
+    case `static`           = "source.decl.attribute.static"
+    case `class`            = "source.decl.attribute.class"
 }
 
 // TODO: Get the possible groups of attribute kinds
-public enum SwiftDeclarationAttributeKindGroup {
+public enum SwiftDeclarationAttributeKindGroup: Int {
     case acl
     case setterACL
     case mutators
     case memoryReference
     case `override`
+    case objcInteroperability
+    case availability
+    case final
+    case interfaceBuilder
+    case typeMethods
 
     var swiftDeclarationAttributeKinds: [SwiftDeclarationAttributeKind] {
         switch self {
@@ -69,19 +78,45 @@ public enum SwiftDeclarationAttributeKindGroup {
         case .mutators:
             return [.mutating,
                     .nonmutating]
-        case .memoryReference:
-            return [.weak]
         case .override:
             return [.override]
+        case .memoryReference:
+            return [.weak]
+        case .objcInteroperability:
+            return [.objc,
+                    .nonobjc,
+                    .objcMembers]
+        case .availability:
+            return [.available]
+        case .final:
+            return [.final]
+        case .interfaceBuilder:
+            return [.ibaction,
+                    .iboutlet,
+                    .ibdesignable,
+                    .ibinspectable]
+        case .typeMethods:
+            return [.static,
+                    .class]
         }
     }
 
     static var allValues: [SwiftDeclarationAttributeKindGroup] {
-        return [.acl, .setterACL, .mutators, .override]
+        return [.acl,
+                .setterACL,
+                .mutators,
+                .override,
+                .memoryReference,
+                .objcInteroperability,
+                .availability,
+                .final,
+                .interfaceBuilder,
+                .typeMethods]
     }
 }
 
 public struct ModifiersOrderRule: ASTRule, OptInRule, ConfigurationProviderRule {
+
     public init() { }
     public var configuration = ModifiersOrderConfiguration(beforeACL: ["override"], afterACL: [])
     public static let description = RuleDescription(
@@ -90,67 +125,57 @@ public struct ModifiersOrderRule: ASTRule, OptInRule, ConfigurationProviderRule 
         description: "Modifiers order should be consistent.",
         kind: .style,
         nonTriggeringExamples: [
-            "@objc \n override public private(set) weak var foo: Bar?\n @objc \npublic final class MyClass: NSObject {\n }",
+            "public static let nnumber = 3 \n",
+            "@objc \npublic final class MyClass: NSObject {\n }",
+            "@objc \n override public private(set) weak var foo: Bar?\n",
+            "@objc \npublic final class MyClass: NSObject {\n }",
             "@objc \npublic final class MyClass: NSObject {\n" +
                 "private final func myFinal() {}\n" +
                 "weak var myWeak: NSString? = nil\n" +
             "public static let nnumber = 3 \n }",
-
             "public final class MyClass {}"
         ],
         triggeringExamples: [
+            "static public let nnnumber = 3 \n",
             "@objc \npublic final class MyClass: NSObject {\n" +
             "final private func myFinal() {}\n}",
-
             "@objc \nfinal public class MyClass: NSObject {}\n",
-
             "final public class MyClass {}\n",
-
             "class MyClass {" +
             "weak internal var myWeak: NSString? = nil\n}",
-
             "class MyClass {" +
             "static public let nnumber = 3 \n }"
         ]
     )
 
-    private let observedDeclarationKinds: [SwiftDeclarationKind] =  [.class,
-                                                                     .enum,
-                                                                     .extension,
-                                                                     .protocol,
-                                                                     .functionConstructor,
-                                                                     .functionDestructor,
-                                                                     .functionMethodClass,
-                                                                     .functionMethodInstance,
-                                                                     .functionMethodStatic,
-                                                                     .functionOperator,
-                                                                     .functionSubscript,
-                                                                     .struct,
-                                                                     .varClass,
-                                                                     .varGlobal,
-                                                                     .varInstance,
-                                                                     .varLocal,
-                                                                     .varParameter,
-                                                                     .varStatic]
+    enum UnrecoginzedModifierKeywords: String {
+        case `static` = "static"
+        case `class` = "class"
+    }
 
     public func validate(file: File,
-                         kind: SwiftDeclarationKind,
-                         dictionary: [String: SourceKitRepresentable]) -> [StyleViolation] {
+                          kind: SwiftDeclarationKind,
+                          dictionary: [String: SourceKitRepresentable]) -> [StyleViolation] {
 
-        guard observedDeclarationKinds.contains(kind),
-              let offset = dictionary.offset else {
+        guard let offset = dictionary.offset else {
             return []
         }
+        
 
-        let preferedOrderOfModifiers = configuration.beforeACL
-                                       .compactMap { return group(of: $0) }
-                                       + [.acl]
-                                       + configuration.afterACL
-                                       .compactMap { return group(of: $0) }
+        let updatedDictionary = updateIfNeeded(in: file, dictionary: dictionary, for: [.class, .static])
 
-        for (index, group) in preferedOrderOfModifiers.enumerated() {
-            guard let groupIndex = findIndex(of: group, in: dictionary.enclosedSwiftAttributesWithMetaData) else { continue }
-            if groupIndex != index {
+        let preferedOrderOfModifiers = [.objcInteroperability, .interfaceBuilder]
+            + configuration.beforeACL
+                .compactMap { return group(of: $0) }
+            + [.acl]
+            + configuration.afterACL
+                .compactMap { return group(of: $0) }
+
+        let modifierGroupsInDeclaration = findModifierGroups(in: updatedDictionary.enclosedSwiftAttributesWithMetaData)
+        let filteredPreferedOrderOfModifiers = preferedOrderOfModifiers.filter { return modifierGroupsInDeclaration.contains($0) }
+
+        for (index, preferedGroup) in filteredPreferedOrderOfModifiers.enumerated() {
+            if preferedGroup != modifierGroupsInDeclaration[index] {
                 return [StyleViolation(ruleDescription: type(of: self).description,
                                        severity: configuration.severityConfiguration.severity,
                                        location: Location(file: file, byteOffset: offset))]
@@ -160,7 +185,17 @@ public struct ModifiersOrderRule: ASTRule, OptInRule, ConfigurationProviderRule 
         return []
     }
 
-    private func findIndex(of group: SwiftDeclarationAttributeKindGroup, in declarationAttributes: [[String: SourceKitRepresentable]]) -> Int? {
+    private func findModifierGroups(in declarationAttributes: [[String: SourceKitRepresentable]]) -> [SwiftDeclarationAttributeKindGroup] {
+        return orderAndTransform(declarationAttributes: declarationAttributes)
+            .compactMap {
+                for declarationAttributeKindGroup in SwiftDeclarationAttributeKindGroup.allValues where declarationAttributeKindGroup.swiftDeclarationAttributeKinds.contains($0) {
+                    return declarationAttributeKindGroup
+                }
+                return nil
+        }
+    }
+
+    private func orderAndTransform(declarationAttributes: [[String: SourceKitRepresentable]]) -> [SwiftDeclarationAttributeKind] {
         return declarationAttributes
             .sorted {
                 guard let rhsOffset = $0["key.offset"] as? Int64, let lhsOffset = $1["key.offset"] as? Int64 else { return false }
@@ -169,10 +204,7 @@ public struct ModifiersOrderRule: ASTRule, OptInRule, ConfigurationProviderRule 
             .compactMap { modifierOrMetaData -> SwiftDeclarationAttributeKind? in
                 guard let attribute = modifierOrMetaData["key.attribute"] as? String else { return nil }
                 return SwiftDeclarationAttributeKind(rawValue: attribute)
-            }
-            .index {
-                group.swiftDeclarationAttributeKinds.contains($0)
-            }
+        }
     }
 
     private func group(of rawAttribute: String) -> SwiftDeclarationAttributeKindGroup? {
@@ -182,5 +214,44 @@ public struct ModifiersOrderRule: ASTRule, OptInRule, ConfigurationProviderRule 
             }
         }
         return nil
+    }
+
+    // TODO: What to do with these hacks only for static and class modifiers?
+    private func updateIfNeeded(in file: File,
+                                dictionary: [String: SourceKitRepresentable],
+                                for keywords: [UnrecoginzedModifierKeywords]) -> [String: SourceKitRepresentable] {
+
+        guard dictionary.kind != SwiftDeclarationKind.class.rawValue else { return dictionary }
+        var updatedDictionary = dictionary
+        for keyword in keywords {
+            let matches = find(keyword: keyword, in: file)
+            let matchedDeclaration = matches.first {
+                guard let nameOffset = dictionary.nameOffset else { return false }
+                return $0.range.contains(nameOffset)
+            }
+
+            // Fake a class or static attribute in the structure dictionary
+            if let match = matchedDeclaration {
+                let attribute: [String: SourceKitRepresentable] = ["key.attribute": SwiftDeclarationAttributeKind.static.rawValue,
+                                                                   "key.offset": Int64(match.range.location),
+                                                                   "key.length": Int64(6)]
+                if var attributes = updatedDictionary["key.attributes"] as? [[String: SourceKitRepresentable]] {
+                    attributes.append(attribute)
+                    updatedDictionary["key.attributes"] = attributes
+                }
+            }
+        }
+
+        return updatedDictionary
+    }
+
+    private func find(keyword: UnrecoginzedModifierKeywords, in file: File) -> [NSTextCheckingResult] {
+        // Pattern idea: match everything if it contains with static or class until the first newline
+        let pattern = "(?:\(keyword.rawValue))(?:.*(\\n|,|\\{|))"
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: []) else { return [] }
+        let fileRange = NSRange(location: 0, length: file.contents.bridge().length)
+        let matches = regex.matches(in: file.contents, options: [], range: fileRange)
+
+        return matches
     }
 }
